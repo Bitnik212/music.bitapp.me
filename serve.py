bf7aecb6e226c421dec42d514abfeddbb4abf2aa
Bit <minikraft1212@gmail.com>
2020-10-18 16:30:26 +0300

    добавил echo

--- serve.py
+++ serve.py
@@ -13,6 +13,22 @@ def connect(sid, environ):
 
 
 @sio.event
+def begin_chat(sid):
+    sio.enter_room(sid, 'chat_user')
+    print('user: ', sid, ' enter room chat_user')
+
+@sio.event
+def exit_chat(sid):
+    sio.leave_room(sid, 'chat_user')
+    print('user: ', sid, ' leave room chat_user')
+
+
+@sio.event
+async def echo(sid, data):
+    await sio.emit('echo', data)
+    print('echo')
+
+@sio.event
 def disconnect(sid):
     print('disconnected on', datetime.now().time())
 
